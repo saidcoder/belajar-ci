9174da248a8b71aa5e64407fbffdf667c3e1a250
saidcyber <saidgrmc16@gmail.com>
2022-02-21 03:22:02 +0700

    menambahkan folder templates untuk view auth_header, auth_footer.

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -5,7 +5,9 @@ class Auth extends CI_Controller {
 
 	public function index()
 	{
+		$this->load->view('auth/template/auth_header');
 		$this->load->view('auth/login');
+		$this->load->view('auth/template/auth_footer');
 
 	}
 }
